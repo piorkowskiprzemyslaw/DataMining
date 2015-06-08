7770b60d99b55ea4f7388cd8f386675cc6f6ff02
dh- <damian-herman@o2.pl>
2015-06-08 14:45:21 +0200

    Added flags for main program flow. Added RC1 for project documentation.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 
 #include "CHI/CHIReduction.h"
 #include "DFT/DFTReduction.h"
@@ -9,6 +10,14 @@
 #include "Data/DataLoader.h"
 #include "KNNClassifier/KNNClassifier.h"
 #include "MI/MIReduction.h"
+
+enum RUNMODE {
+    RM_DEF = 1,
+    RM_DFT = 2,
+    RM_MI = 4,
+    RM_CHI = 8,
+    RM_K = 16
+};
 
 template<typename T>
 std::shared_ptr<Data> loadData(T &&dataLoader, const std::string &fileName, bool readHeaders) {
@@ -33,6 +42,37 @@ void printNumberOfFaults(const std::vector<int>& classes, std::shared_ptr<Data> 
     std::cout << "Faults : " << faults << std::endl;
 }
 
+void printQuality(const std::vector<int>& classes, std::shared_ptr<Data> test_data) {
+    auto faults = 0u;
+    auto true_positive = 0u;
+    auto true_negative = 0u;
+    auto false_positive = 0u;
+    auto false_negative = 0u;
+    float precision = 0.0;
+    float recall = 0.0;
+
+    for(size_t i = 0u; i < classes.size(); ++i) {
+        if(classes[i]!= test_data->getRow(i)[57]) { //incorrectly...
+            if (classes[i] == 1) ++false_positive; //identified
+            else ++false_negative; //rejected
+            ++faults;
+        }
+        else { //Correctly...
+            if (classes[i] == 1) ++true_positive; //identified (as spam)
+            else ++true_negative; //rejected
+        }
+    }
+    precision = (float)true_positive / (true_positive + false_positive);
+    recall = (float)true_positive / (true_positive + false_negative);
+
+    std::cout << "Faults: " << faults << "; ";
+    std::cout << "TP: " << true_positive << "; ";
+    std::cout << "TN: " << true_negative << "; ";
+    std::cout << "FP: " << false_positive << "; ";
+    std::cout << "FN: " << false_negative << "; ";
+    std::cout << "Precision: " << precision << "; Recall: " << recall << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
     // default train data file name
@@ -55,6 +95,8 @@ int main(int argc, char* argv[])
     CHIReduction::ReductionMode CHI_REDUCTION_TYPE = CHIReduction::ReductionMode::Maximum;
     // mi algo reduction treshold
     double CHI_THRESHOLD = 0.2;
+    //program mode
+    unsigned ALGORITHMS = RM_DEF;
 
 
     for( int i = 1 ; i < argc ; ++i){
@@ -68,22 +110,30 @@ int main(int argc, char* argv[])
             CLASS_NAME = argv[++i];
         } else if( strcmp(argv[i], "-k") == 0 ) {
             K = std::stoi(argv[++i]);
+            ALGORITHMS |= RM_K;
         } else if( strcmp(argv[i], "-DFT") == 0 ) {
             DFT_TRESHOLD = std::stod(argv[++i]);
+            ALGORITHMS |= RM_DFT;
         } else if( strcmp(argv[i], "-MI_MAX") == 0 ) {
             MI_REDUCTION_TYPE = MIReductionType::MAX;
+            ALGORITHMS |= RM_MI;
         } else if( strcmp(argv[i], "-MI_AVG") == 0 ) {
             MI_REDUCTION_TYPE = MIReductionType::AVG;
+            ALGORITHMS |= RM_MI;
         } else if( strcmp(argv[i], "-MI") == 0 ) {
             MI_TRESHOLD = std::stod(argv[++i]);
+            ALGORITHMS |= RM_MI;
         } else if( strcmp(argv[i], "-l") == 0 ) {
             Logger::setLogLevel(std::stoi(argv[++i]));
         } else if( strcmp(argv[i], "-CHI_MAX") == 0 ) {
             CHI_REDUCTION_TYPE = CHIReduction::ReductionMode::Maximum;
+            ALGORITHMS |= RM_CHI;
         } else if( strcmp(argv[i], "-CHI_AVG") == 0 ) {
             CHI_REDUCTION_TYPE = CHIReduction::ReductionMode::Average;
+            ALGORITHMS |= RM_CHI;
         } else if( strcmp(argv[i], "-CHI") == 0 ) {
             CHI_THRESHOLD = std::stod(argv[++i]);
+            ALGORITHMS |= RM_CHI;
         }
     }
 
@@ -96,32 +146,42 @@ int main(int argc, char* argv[])
     KNNClassifier classif(train_data, K);
     classif.setTestData(test_data);
 
-    // without reduction
-    const std::vector<double> w1(test_data->nAttributes(), 1.0);
-    const auto classes = classif.classifiy(w1);
-    printNumberOfFaults(classes,test_data);
+    if (ALGORITHMS & RM_K){
+        // without reduction
+        LOG(INFO) << "kNN:";
+        const std::vector<double> w1(test_data->nAttributes(), 1.0);
+        const auto classes = classif.classifiy(w1);
+        printQuality(classes,test_data);
+    }
 
-    // dft reduction
-    DFTReduction dft(train_data);
-    dft.setTreshold(DFT_TRESHOLD);
-    const std::vector<double> w2 = dft.reduceAttributes();
-    const auto classes_dft = classif.classifiy(w2);
-    printNumberOfFaults(classes_dft, test_data);
-
-    // mi reduction
-    MIReduction mi(train_data);
-    mi.setReductionType(MI_REDUCTION_TYPE);
-    mi.setTreshold(MI_TRESHOLD);
-    const std::vector<double> w3 = mi.reduceAttributes();
-    const auto classes_mi = classif.classifiy(w3);
-    printNumberOfFaults(classes_mi, test_data);
-
-    // chi reduction
-    CHIReduction chi(train_data);
-    chi.setThreshold(CHI_THRESHOLD);
-    const auto w4 = chi.reduce(CHI_REDUCTION_TYPE);
-    const auto classes_chi = classif.classifiy(w4);
-    printNumberOfFaults(classes_chi, test_data);
+    if (ALGORITHMS & RM_DFT){
+        // dft reduction
+        LOG(INFO) << "DFT:";
+        DFTReduction dft(train_data);
+        dft.setTreshold(DFT_TRESHOLD);
+        const std::vector<double> w2 = dft.reduceAttributes();
+        const auto classes_dft = classif.classifiy(w2);
+        printQuality(classes_dft, test_data);
+    }
+    if (ALGORITHMS & RM_MI){
+        // mi reduction
+        LOG(INFO) << "MI:";
+        MIReduction mi(train_data);
+        mi.setReductionType(MI_REDUCTION_TYPE);
+        mi.setTreshold(MI_TRESHOLD);
+        const std::vector<double> w3 = mi.reduceAttributes();
+        const auto classes_mi = classif.classifiy(w3);
+        printQuality(classes_mi, test_data);
+    }
+    if (ALGORITHMS & RM_CHI){
+        // chi reduction
+        LOG(INFO) << "CHI:";
+        CHIReduction chi(train_data);
+        chi.setThreshold(CHI_THRESHOLD);
+        const auto w4 = chi.reduce(CHI_REDUCTION_TYPE);
+        const auto classes_chi = classif.classifiy(w4);
+        printQuality(classes_chi, test_data);
+    }
 
     return 0;
 }
